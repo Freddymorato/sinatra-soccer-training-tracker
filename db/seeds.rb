8693f43b9ced8b1aa70e220d5f91c64b14148e6d
Frederico  Morato <freddymorato@gmail.com>
2018-02-24 05:15:29 +0000

    created and ran db:seed file

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Category.create(name: "Shooting")
+Category.create(name: "Passing")
+Category.create(name: "Heading")
+Category.create(name: "Tackling")
+Category.create(name: "Dribbling")
+Category.create(name: "Conditioning")
