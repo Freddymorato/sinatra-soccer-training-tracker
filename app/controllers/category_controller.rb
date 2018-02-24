90c104504e1b230e7c2227276e519c57de4b10a9
Frederico  Morato <freddymorato@gmail.com>
2018-02-24 19:11:00 +0000

    updated /categories/:category route

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,7 +1,18 @@
+require 'pry'
+require 'rack-flash'
 
 class CategoryController < ApplicationController
+  use Rack::Flash
 
   get '/categories/:category' do
-    
+    if logged_in?
+      @category = Category.find_by(:name => params[:category])
+      @drills =  @category.drills
+      @drills.order! 'created_at DESC'
+      erb :'/categories/show_by_cat'
+    else
+      flash[:message] = "You cannot view that page until you have logged in. Please login to continue."
+      redirect '/login'
+    end
   end
 end
